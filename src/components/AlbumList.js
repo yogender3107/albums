385283cc609b9b1d276a3515c5cbfd8ff8ba0f4d
yogender3107 <yogender3107@gmail.com>
2018-12-30 01:04:16 +0530

    card layout in app

---
+++ src/components/AlbumList.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
+
+class AlbumList extends Component {
+  constructor() {
+    super();
+    this.state = { albums: [] };
+  }
+  componentDidMount() {
+    axios
+      .get("https://rallycoding.herokuapp.com/api/music_albums")
+      .then(response => this.setState({ albums: response.data }));
+  }
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
+  }
+  render() {
+    return <View>{this.renderAlbums()}</View>;
+  }
+}
+
+export default AlbumList;
